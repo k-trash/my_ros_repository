e0def83f75066da71ea519b1bd834b04be933420
k-trash <d.kintou@gmail.com>
2021-03-07 18:42:53 +0900

    offset

--- imu_test/src/cpp_serial_test.cpp
+++ imu_test/src/cpp_serial_test.cpp
@@ -1,19 +1,25 @@
-//Ver1.1.0 2020/03/07 k-trash
-//change to ros node
+//Ver1.1.1 2020/03/07 k-trash
+//add magnet publish
 
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
+#include "sensor_msgs/MagneticField.h"
 
 #include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
 
+//#define OFFSET
+
 int openSerial(const char *devise_name_);
 
 int main(int argc, char *argv[]){
 	ros::init(argc, argv, "cpp_serial_test");
 	ros::NodeHandle nh;
+
+	int max[3] = {0};
+	int min[3] = {0};
 
 	int ret_dev = 0;
 	int rec_write = 0;
@@ -23,8 +29,10 @@ int main(int argc, char *argv[]){
 	unsigned char recv_data[256] = {0};
 
 	sensor_msgs::Imu imu;
-	
-	ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("imu_data", 10);
+	sensor_msgs::MagneticField mag;
+
+	ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("imu/data_raw", 10);
+	ros::Publisher mag_pub = nh.advertise<sensor_msgs::MagneticField>("imu/mag", 10);
 
 	ret_dev = openSerial(device_name);
 
@@ -49,15 +57,35 @@ int main(int argc, char *argv[]){
 				imu.linear_acceleration.x = int16_t(recv_data[0]<<8 | recv_data[1]);
 				imu.linear_acceleration.y = int16_t(recv_data[2]<<8 | recv_data[3]);
 				imu.linear_acceleration.z = int16_t(recv_data[4]<<8 | recv_data[5]);
-				imu.angular_velocity.x = int16_t(recv_data[6]<<8 | recv_data[7]);
-				imu.angular_velocity.y = int16_t(recv_data[8]<<8 | recv_data[9]);
-				imu.angular_velocity.z = int16_t(recv_data[10]<<8 | recv_data[11]);
-				std::cout << std::endl;
+				imu.angular_velocity.x = int16_t(recv_data[6]<<8 | recv_data[7]) + 40;
+				imu.angular_velocity.y = int16_t(recv_data[8]<<8 | recv_data[9]) - 40;
+				imu.angular_velocity.z = int16_t(recv_data[10]<<8 | recv_data[11]) - 25;
+				
+				mag.header.frame_id = "imu_link";
+				mag.header.stamp = ros::Time::now();
+				mag.magnetic_field.x = int16_t(recv_data[12]<<8 | recv_data[13]) + 200;
+				mag.magnetic_field.y = int16_t(recv_data[14]<<8 | recv_data[15]) + 100;
+				mag.magnetic_field.z = int16_t(recv_data[16]<<8 | recv_data[17]);
+				
+				#ifdef OFFSET
+					if(max[0] < mag.magnetic_field.x)	max[0] = mag.magnetic_field.x;
+					if(min[0] > mag.magnetic_field.x)	min[0] = mag.magnetic_field.x;
+					if(max[1] < mag.magnetic_field.y)	max[1] = mag.magnetic_field.y;
+					if(min[1] > mag.magnetic_field.y)	min[1] = mag.magnetic_field.y;
+					if(max[2] < mag.magnetic_field.z)	max[2] = mag.magnetic_field.z;
+					if(min[2] > mag.magnetic_field.z)	min[2] = mag.magnetic_field.z;
+
+					for(int i=0;i<3;i++){
+						std::cout << max[i] << '\t' << min[i] << '\t';
+					}
+					std::cout << std::endl;
+				#endif
 			}else{
 				std::cout << "Read fail" << std::endl;
 			}
 		}
 		imu_pub.publish(imu);
+		mag_pub.publish(mag);
 
 		ros::spinOnce();
 		loop_rate.sleep();
